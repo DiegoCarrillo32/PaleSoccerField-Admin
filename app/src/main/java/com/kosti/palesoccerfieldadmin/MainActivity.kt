248d5b32137c7f25c208bbae87b6983f33d37782
Diego Carrillo <diegocarrilloa@outlook.com>
2023-09-24 18:46:46 -0600

    Revert "Merge branch 'feature/PAL-8' into develop"
    This reverts commit 4f71a46d1b8b7b3813691ba3dc97831b9b225644.

--- app/src/main/java/com/kosti/palesoccerfieldadmin/MainActivity.kt
+++ app/src/main/java/com/kosti/palesoccerfieldadmin/MainActivity.kt
@@ -1,24 +1,11 @@
 package com.kosti.palesoccerfieldadmin
 
-
-
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import com.kosti.palesoccerfieldadmin.UserListPackage.UserList
-
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navigateButtonTest: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        navigateButtonTest = findViewById(R.id.navigationTestButton)
-        navigateButtonTest.setOnClickListener {
-            val intent = Intent(this, UserList::class.java)
-            startActivity(intent)
-        }
     }
 }
--- app/src/main/java/com/kosti/palesoccerfieldadmin/UserListPackage/UserListDataModel.kt
+++
@@ -1,9 +0,0 @@
-package com.kosti.palesoccerfieldadmin.UserListPackage
-
-public class UserListDataModel(name: String, clasf:String, pos: String, nick: String) {
-    var Name:String = name
-    var Nickname: String = nick
-    var Clasification: String = clasf;
-    var Position: String = pos;
-
-}
