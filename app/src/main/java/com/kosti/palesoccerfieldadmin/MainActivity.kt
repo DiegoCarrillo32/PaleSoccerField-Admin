6e2c0cb517c02d91e94d68e0b2f21adc89d0a9a4
Julio Cesar Castro <julioCC@estudiantec.cr>
2023-10-08 22:40:24 -0600

    Registro funcional

--- app/src/main/java/com/kosti/palesoccerfieldadmin/MainActivity.kt
+++ app/src/main/java/com/kosti/palesoccerfieldadmin/MainActivity.kt
@@ -3,17 +3,22 @@ package com.kosti.palesoccerfieldadmin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import com.kosti.palesoccerfieldadmin.aproveUsers.AproveUsers
+import com.kosti.palesoccerfieldadmin.registro.Register
 import com.kosti.palesoccerfieldadmin.userListPackage.UserList
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnRegistrarUsuario: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnRegistrarUsuario = findViewById(R.id.registrarUsuarios)
 
-        var btnNavegar = findViewById<Button>(R.id.btnBuscarUsuarios)
+        val btnNavegar = findViewById<Button>(R.id.btnBuscarUsuarios)
         btnNavegar.setOnClickListener {
             val intent = Intent(this, UserList::class.java)
             startActivity(intent)
@@ -25,5 +30,15 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        btnRegistrarUsuario.setOnClickListener{
+            toRegister()
+        }
+
+    }
+
+    fun toRegister() {
+        val intent = Intent(this, Register::class.java)
+        startActivity(intent)
+        finish()
     }
 }
