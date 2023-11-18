2d17577a6caae13d58732b629fdf1e75372d374b
AndrikSJD <andriksj2208@gmail.com>
2023-11-18 14:48:41 -0600

    create reservations

---
+++ app/src/main/java/com/kosti/palesoccerfieldadmin/reservations/createReservations/CreateReservations.kt
@@ -0,0 +1,12 @@
+package com.kosti.palesoccerfieldadmin.reservations.createReservations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kosti.palesoccerfieldadmin.R
+
+class CreateReservations : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_reservations)
+    }
+}
