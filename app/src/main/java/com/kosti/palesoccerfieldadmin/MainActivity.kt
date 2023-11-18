492ba83cb1b9dcf0fd44f2ac756a7f2d7fdb753f
jQuesada06 <josucho.45@estudiantec.cr>
2023-11-18 15:18:24 -0600

    Update

--- app/src/main/java/com/kosti/palesoccerfieldadmin/MainActivity.kt
+++ app/src/main/java/com/kosti/palesoccerfieldadmin/MainActivity.kt
@@ -8,6 +8,7 @@ import com.kosti.palesoccerfieldadmin.aproveReservations.AproveReservations
 import com.kosti.palesoccerfieldadmin.aproveUsers.AproveUsers
 import com.kosti.palesoccerfieldadmin.deletePassword.DeleteAccount
 import com.kosti.palesoccerfieldadmin.login.Login
+import com.kosti.palesoccerfieldadmin.macthBookings.MatchBookings
 import com.kosti.palesoccerfieldadmin.registro.Register
 import com.kosti.palesoccerfieldadmin.schedules.Schedules
 import com.kosti.palesoccerfieldadmin.userAdminProfile.EditUserData
@@ -25,6 +26,11 @@ class MainActivity : AppCompatActivity() {
 
         val userId = intent.getStringExtra("userId").toString()
 
+        val btnBooking = findViewById<Button>(R.id.btnReservas)
+        btnBooking.setOnClickListener {
+            val intent = Intent(this, MatchBookings::class.java)
+            startActivity(intent)
+        }
 
         val btnNavegar = findViewById<Button>(R.id.btnBuscarUsuarios)
         btnNavegar.setOnClickListener {
