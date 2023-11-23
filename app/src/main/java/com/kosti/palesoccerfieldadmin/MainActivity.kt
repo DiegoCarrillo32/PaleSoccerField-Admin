7200d86c6d63e2e65dbef1eb081483af9f1695bc
Julio Cesar Castro <julioCC@estudiantec.cr>
2023-11-22 21:01:54 -0600

    Registro ahora permite las posiciones

--- app/src/main/java/com/kosti/palesoccerfieldadmin/MainActivity.kt
+++ app/src/main/java/com/kosti/palesoccerfieldadmin/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import com.kosti.palesoccerfieldadmin.approveReview.AprobarResenia
@@ -31,6 +32,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnLogOut: Button
     lateinit var usuarioNombreTV:TextView
     lateinit var usuarioCorreoTV: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
